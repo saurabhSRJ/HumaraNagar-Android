64facf96d4c66af946d83f6addd2ed298be8007b
Saurabh Jaiswal <saurabhjaiswal@192.168.1.177>
2023-06-12 16:29:39 +0530

    text and image post details done

--- app/src/main/java/com/humara/nagar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/humara/nagar/ui/home/HomeFragment.kt
@@ -4,36 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.humara.nagar.R
+import com.humara.nagar.adapter.PostAdapter
 import com.humara.nagar.analytics.AnalyticsData
 import com.humara.nagar.base.BaseFragment
 import com.humara.nagar.databinding.FragmentHomeBinding
+import com.humara.nagar.utils.setNonDuplicateClickListener
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class HomeFragment : BaseFragment() {
-
+class PostFragment : BaseFragment() {
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
+    private val postAdapter: PostAdapter by lazy {
+        PostAdapter()
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.run {
+            ivNotification.setNonDuplicateClickListener {
+//                findNavController().navigate(R.id.fullImagePreviewFragment, bundleOf("imagesList" to arrayOf("")))
+                findNavController().navigate(PostFragmentDirections.actionHomeFragmentToFullImagePreviewFragment(arrayOf(""), getScreenName()))
+            }
+            rvPost.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = postAdapter
+            }
+            swipeRefresh.setOnRefreshListener {
+                lifecycleScope.launch {
+                    delay(2000)
+                    postAdapter.shuffleList()
+                    swipeRefresh.isRefreshing = false
+                }
+            }
+            // Configure the refreshing colors
+            swipeRefresh.setColorSchemeResources(R.color.blue_4285F4,
+                R.color.stroke_green,
+                R.color.stroke_yellow,
+                R.color.stroke_red);
         }
-        return root
     }
 
     override fun onDestroyView() {
